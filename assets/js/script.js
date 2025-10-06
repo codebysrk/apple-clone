b0dd633f1de1ffc5d01bc1950c622719e9d93060
codebysrk <maishahrukhh@gmail.com>
2025-10-06 13:51:15 +0530

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const swiper = new Swiper('.swiper', {
   // Default (Desktop) settings
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1.5, // Show 3 slides on desktop
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
@@ -16,14 +16,14 @@ const swiper = new Swiper('.swiper', {
 
   // Responsive breakpoints
   breakpoints: {
-    // when window width is <= 768px
+    // when window width is <= 768px (Tablet)
     768: {
-      slidesPerView: 2,
+      slidesPerView: 2, // Show 2 slides on tablets
       spaceBetween: 20
     },
-    // when window width is <= 480px
+    // when window width is <= 480px (Mobile)
     480: {
-      slidesPerView: 1,
+      slidesPerView: 1.2, // Show 1 and a bit of the next on mobile
       spaceBetween: 10
     }
   }
